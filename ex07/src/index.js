d73cbf423748296e7428bca7f3f62c3ca43fa29c
mirza-z <zaimovic330@gmail.com>
2022-04-01 17:08:32 +0200

    Added ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var myName = "Mirza";
+    var myString = "My name is " + myName + ", how are you?";
+    // Only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
