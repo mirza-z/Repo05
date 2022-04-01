be24747fa12e13d7f9d390456de210f039e44520
mirza-z <zaimovic330@gmail.com>
2022-04-01 16:27:56 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myFirstName = "Mirza";
+    var myLastName = "Zaimovic";
+    var myHomeTown = "Breza";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
