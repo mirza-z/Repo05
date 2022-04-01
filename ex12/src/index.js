7d693f5141e234b21671160dc2d3f0c39fe58a3f
mirza-z <zaimovic330@gmail.com>
2022-04-01 17:18:50 +0200

    Added ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Tesla";
+    var thirdLetterOfLastNamme = lastName[2];
+    // Only change code above this line
+    return thirdLetterOfLastNamme;
+}
+
+console.log(main());
+module.exports = main;
